9ac7e6dcc01140cd4ee8a883b68edf52f495eb3a
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-06-11 02:30:08 -0600

    primer implemento de ssr en front

--- src/components/registeredPersons/RegisteredPersons.tsx
+++ src/components/registeredPersons/RegisteredPersons.tsx
@@ -1,5 +1,5 @@
 "use client";
-//short
+
 import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./RegisteredPersons.module.css";
@@ -8,24 +8,37 @@ export const RegisteredPersons = () => {
   const [userCount, setUserCount] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      axios
-        .get("http://localhost:3001/usuarios")
-        .then((response) => {
-          setUserCount(response.data.length);
-        })
-        .catch((error) => {
-          console.error("There was an error!", error);
-        });
-    }, 5000); // Actualizaxion
+    // Solicitud inicial para obtener el número actual de usuarios registrados (priueba)
+    axios
+      .get("http://localhost:3001/usuarios")
+      .then((response) => {
+        setUserCount(response.data.length);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the initial user count!", error);
+      });
 
-    return () => clearInterval(interval); // reset
-  }, []); 
+    const eventSource = new EventSource("http://localhost:3001/usuarios/esperarNotificaciones");
+    
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setUserCount(data.totalUsuarios);
+    };
+
+    eventSource.onerror = (error) => {
+      console.error("There was an error with the SSE connection:", error);
+      eventSource.close();
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
 
   return (
     <div className={styles.contenedor}>
       <h1 className="text-3xl font-bold text-blue-500 hover:text-blue-700 transition-colors duration-300">
-        Número de usuarios actualizados cada 5 segundos
+        Número de usuarios actualizados en tiempo real
       </h1>
       <p className={styles.let}>Total de usuarios: {userCount}</p>
     </div>
